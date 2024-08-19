f6eab3a1bfb8e691c4c1390585d3873d7e58f196
Vashistht <60758917+Vashistht@users.noreply.github.com>
2024-08-19 21:51:53 +0000

    Deploying to gh-pages from @ Vashistht/vashistht.github.io@b9d3cb47b03d791a27fa6115f7f566ad23164084 🚀

---
+++ readme_extra.sh
@@ -0,0 +1,5 @@
+# how to install and deploy locally if you want to 
+bundle install     
+bundle exec jekyll serve   # to see changes locally
+# bin/deploy --user             
+# bundle exec jekyll build # to deploy
