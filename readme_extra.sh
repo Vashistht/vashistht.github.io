02264637679a2ace0a4f9600641bfe9bbce1cb5d
Vashistht <60758917+Vashistht@users.noreply.github.com>
2026-04-23 15:56:05 +0000

    Deploying to gh-pages from @ Vashistht/vashistht.github.io@f4aad2dd377287b55df28858e63548a20058ccfa 🚀

--- readme_extra.sh
+++ readme_extra.sh
@@ -4,8 +4,16 @@ bundle add logger
 brew install imagemagick
 bundle install
 bundle exec jekyll serve
-# how to install and deploy locally if you want to 
-bundle install     
+# how to install and deploy locally if you want to
+bundle install
 bundle exec jekyll serve   # to see changes locally
-# bin/deploy --user             
+# bin/deploy --user
 # bundle exec jekyll build # to deploy
+
+# brew install ruby
+# echo 'export PATH="/opt/homebrew/opt/ruby/bin:$PATH"' >> ~/.zshrc
+# source ~/.zshrc
+# ruby -v
+# gem install bundler:2.5.7
+# bundle install
+# bundle exec jekyll serve
